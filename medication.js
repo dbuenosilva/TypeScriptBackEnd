4209c3b62dd40bc2d486f64788626c901d086765
Diego Bueno <diego@gwaya.com>
2021-07-30 14:58:19 +1000

    Medication page

---
+++ medication.js
@@ -0,0 +1,42 @@
+/*
+##########################################################################
+# Project: PROG6003 - Programming Mobile and Cloud Systems - Assigment 1
+# File: medication.ts
+# Author: Diego Bueno - d.bueno.da.silva.10@student.scu.edu.au
+# Date: 30/07/2021
+# Description: Backend type-script to the Medication control page.
+#
+##########################################################################>
+# Changelog
+# Author:
+# Date:
+# Description:
+#
+##########################################################################>*/
+// Defining a array to save the medication control objects.
+var medicationRecords = new Array();
+// Creating the ts variable table to access the html content
+var viewMedicationTable = document.getElementById("MedicationsTable");
+// Method to add a new medication
+function addMedication() {
+    // Define local variables and casting the content from HTML file.
+    var identifier = Number(document.getElementById("identifier").value);
+    var medicationName = String(document.getElementById("medicationName").value);
+    var dailyDose = Number(document.getElementById("dailyDose").value);
+    var numberOfRepeats = Number(document.getElementById("numberOfRepeats").value);
+    // Creating a instance of IMedication
+    var medication = { id: identifier, name: medicationName, dose: dailyDose, repeats: numberOfRepeats };
+    // Adding to new created instance to the list of medications
+    medicationRecords.push(medication);
+    // Creating a obj row element
+    var row = viewMedicationTable.insertRow(viewMedicationTable.rows.length);
+    // Creating objs cells elements in the row created above
+    var idCol = row.insertCell(0);
+    var nameCol = row.insertCell(1);
+    var doseCol = row.insertCell(2);
+    var repeatsCol = row.insertCell(3);
+    idCol.innerHTML = String(identifier);
+    nameCol.innerHTML = medicationName;
+    doseCol.innerHTML = String(dailyDose);
+    repeatsCol.innerHTML = String(numberOfRepeats);
+}
---
+++ medication.ts
@@ -0,0 +1,55 @@
+/*
+##########################################################################
+# Project: PROG6003 - Programming Mobile and Cloud Systems - Assigment 1
+# File: medication.ts
+# Author: Diego Bueno - d.bueno.da.silva.10@student.scu.edu.au 
+# Date: 30/07/2021
+# Description: Backend type-script to the Medication control page.
+#
+##########################################################################>
+# Changelog                            
+# Author:                          
+# Date:                                                        
+# Description:     
+#
+##########################################################################>*/
+
+// Defining a interface for a medication control object
+interface IMedication {id: number, name: String, dose: number, repeats: number}
+
+// Defining a array to save the medication control objects.
+let medicationRecords: Array<IMedication> = new Array<IMedication>();
+
+// Creating the ts variable table to access the html content
+let viewMedicationTable = document.getElementById("MedicationsTable") as HTMLTableElement;
+
+// Method to add a new medication
+function addMedication() {
+
+    // Define local variables and casting the content from HTML file.
+    let identifier: number = Number((document.getElementById("identifier") as HTMLInputElement).value);
+    let medicationName: string = String((document.getElementById("medicationName") as HTMLInputElement).value);
+    let dailyDose: number = Number((document.getElementById("dailyDose") as HTMLInputElement).value);
+    let numberOfRepeats: number = Number((document.getElementById("numberOfRepeats") as HTMLInputElement).value);
+
+    // Creating a instance of IMedication
+    let medication: IMedication = {id: identifier, name: medicationName, dose: dailyDose, repeats: numberOfRepeats}
+
+    // Adding to new created instance to the list of medications
+    medicationRecords.push(medication)
+
+    // Creating a obj row element
+    let row = viewMedicationTable.insertRow(viewMedicationTable.rows.length) as HTMLTableRowElement;
+
+    // Creating objs cells elements in the row created above
+    let idCol      = row.insertCell(0) as HTMLTableCellElement;
+    let nameCol    = row.insertCell(1) as HTMLTableCellElement;
+    let doseCol    = row.insertCell(2) as HTMLTableCellElement;
+    let repeatsCol = row.insertCell(3) as HTMLTableCellElement;
+
+    idCol.innerHTML = String(identifier);
+    nameCol.innerHTML = medicationName;
+    doseCol.innerHTML = String(dailyDose);        
+    repeatsCol.innerHTML = String(numberOfRepeats);    
+    
+}
